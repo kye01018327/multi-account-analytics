7a4fff669bd14a1fdd998d25c989932cc3f08688
Kevin Ye <ranchonpizza24@gmail.com>
2025-11-27 22:25:46 -0800

    chore: add flask server template

--- back-end/app.py
+++ back-end/app.py
@@ -2,11 +2,11 @@ from flask import request, Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, Flask!"
+@app.route('/profiles/<username>', methods=['GET'])
+def get_profile():
+    pass
 
-@app.route('/greet')
-def greet():
-    name = request.args.get('name', 'Guest')  # URL param ?name=John
-    return f"Hello, {name}!"
+@app.route('/profiles', methods=['POST'])
+def create_profile():
+    pass
+
