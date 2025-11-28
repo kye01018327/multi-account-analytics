c2afa72ac68ca26551ac651344e71708e841ecc1
Kevin Ye <ranchonpizza24@gmail.com>
2025-11-28 00:19:01 -0800

    chore: add environment variables

--- back-end/app.py
+++ back-end/app.py
@@ -1,9 +1,21 @@
 from flask import request, Flask
+from utils import Database
+import os
+
+env = os.environ
 
 app = Flask(__name__)
 
+db = Database(
+    env['DB_NAME'], 
+    env['DB_USER'], 
+    env['DB_PASSWORD'], 
+    env['DB_HOST'],
+    env['DB_PORT'])
+
 @app.route('/profiles/<username>', methods=['GET'])
 def get_profile():
+
     pass
 
 @app.route('/profiles', methods=['POST'])
