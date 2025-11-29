4c9923e864c1812e816894c1579ff9f35731f27c
Kevin Ye <ranchonpizza24@gmail.com>
2025-11-28 17:27:22 -0800

    feat: pull profilename from database and display on frontend profile page

--- front-end/src/pages/Login.tsx
+++ front-end/src/pages/Login.tsx
@@ -29,7 +29,7 @@ function ProfileForm() {
             return
         }
         const res = await fetch(`http://127.0.0.1:5000/profiles/${profilename}`)
-        if (res.status == 404) {
+        if (!res.ok) {
             setMessage('Profile does not exist')
             return
         }
--- front-end/src/pages/Profile.tsx
+++ front-end/src/pages/Profile.tsx
@@ -1,12 +1,26 @@
+import { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router'
 
 
 function Profile() {
-    const { username } = useParams()
+    const [profilename, setProfilename] = useState('')
+    const { param } = useParams()
     let navigate = useNavigate()
+    useEffect(() => {
+        async function fetchData() {
+            const res = await fetch(`http://127.0.0.1:5000/profiles/${param}`)
+            if (!res.ok) {
+                setProfilename('ERROR')
+                return
+            }
+            const data = await res.json()
+            setProfilename(data)
+        }
+        fetchData()
+    }, [])
     return (
         <>
-            <div>Hello, { username }</div>
+            <div>Hello, { profilename }</div>
             <button onClick={() => {navigate('/')}}>Log Out</button>
         </>
     )
